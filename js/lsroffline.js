1c9766fd07c6d26ee22de8842267d7f6de69732b
acesmndr <acesmndr@gmail.com>
2014-02-21 17:09:52 +0545

    Offline Check beta2

--- js/lsroffline.js
+++ js/lsroffline.js
@@ -1,5 +1,6 @@
 var arr=[[3,9,12,18],[4,10,14,20],[5,12,16,21],[6,13,17,22],[7,14,18,23],[9,16,19,24],[10,17,20,1]];
-var last_group=333function update(){
+var last_group=3;
+function update(){
 	alert("The update functionality is not possible in your device as it doesn't support localStorage! Please visit our website for the updated version if it is available!");
 	}
 function formupdate(){
@@ -11,4 +12,4 @@ $(function(){
 	$("#showSchedule"+grp).addClass("groupNumberActive");
 	$(".selectDiv").hide(0);
 	$("#community").show(0);
-	});
+});
